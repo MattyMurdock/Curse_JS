e37b66aff22b8ff4d4633a1293783cf23f9e88bb
MattyMurdock <oswaldlazarof.99@gmail.com>
2026-01-06 13:04:43 -0600

    [ADD] Stage:Estructura del código

---
+++ Fundamentos/Inicio.js
@@ -0,0 +1,42 @@
+// Sentencias
+alert('Hola'); alert('Mundo');
+
+// se pueden hacer las sentencias separadas de esta forma
+alert('Hola'); 
+alert('Mundo');
+
+//No es SIEMPRE asi
+alert('Hola') 
+alert('Mundo')
+
+alert(3 +
+1
++2);
+
+
+// Aqui hay un ejemplo de un error !!! 
+
+alert('Hello');
+[1,2].forEach(alert);
+
+alert('Hello')
+[1,2].forEach(alert);
+
+//Este comentario ocupa una linea
+alert('Hello');
+alert('Mensaje'); //Aqui hay otro comentario despues de la sentencia
+
+
+
+/*Esto es un comentario mas largo conchetumare alvgra compa ya se la sapbe pa que se la pla*/
+alert('hola Mundo');
+/* este no se va ejecutar pq esta comentado
+alert('conchetumare');
+*/
+
+
+// Los comentarios anidados no son permitidos
+
+/* 
+	/* comentario anidado ?!? */ 
+*/ alert( 'Mundo' );
