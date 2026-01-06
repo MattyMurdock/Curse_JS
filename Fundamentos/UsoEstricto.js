73c2e93cbee469c875dd7da2f99785a446cb8038
MattyMurdock <oswaldlazarof.99@gmail.com>
2026-01-06 13:32:23 -0600

    [ADD]Stage:Uso del uso estricto

---
+++ Fundamentos/UsoEstricto.js
@@ -0,0 +1,26 @@
+"use strict";
+// este codigo funciona de la manera moderna
+...
+
+//_-----
+
+
+alert("algo de codigo");
+// la directiva "use strict" es ignorada se tiene que usar lo siguiente.
+"use strict";
+// el modo estricto no esta activado.
+
+// FUnciona para la mayoria de los navegadores
+
+'use strict'; <sHIFT+enter pa una nueva linea>
+// el codigo que quieras...
+<intro para ejecutar>
+
+// ALternativa si falla
+
+(function(){
+	'use strict';
+	//aqui el codigo...
+})()
+
+
